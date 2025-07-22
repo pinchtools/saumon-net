2c2a067d71adbebd02d54aff1c5f892f6a2218c6
Vincent Pellé <pelle.vincent@gmail.com>
2025-07-22 14:26:34 +0200

    setup sentry

--- Gemfile
+++ Gemfile
@@ -43,6 +43,10 @@ gem "thruster", require: false
 
 gem "decent_exposure"
 
+# Tracing, debugging
+gem "sentry-ruby"
+gem "sentry-rails"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
---
+++ config/initializers/sentry.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+Sentry.init do |config|
+  config.breadcrumbs_logger = [ :active_support_logger ]
+  config.dsn = Rails.application.credentials.sentry.dsn!
+
+  config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
+
+  # Add data like request headers and IP for users,
+  # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
+  config.send_default_pii = true
+
+  # Set traces_sample_rate to 1.0 to capture 100%
+  # of transactions for tracing.
+  # We recommend adjusting this value in production.
+  config.traces_sample_rate = 1.0
+  # or
+  config.traces_sampler = lambda do |context|
+    true
+  end
+end
