75cddde30766f6f54f2056132c92e02c519b66e3
Vincent Pellé <vincent.pelle@pm.me>
2025-08-08 16:12:03 +0200

    rm checksum from the default factory

--- spec/factories/downloads.rb
+++ spec/factories/downloads.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :download do
     name { Faker::Internet.slug }
     fingerprint { Faker::Code.asin }
-    checksum { "ASZ2" }
     current { true }
     version { 1 }
     source
